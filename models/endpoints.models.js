80eed711bb451d8fe2041e35c6825df3f9653bf5
MaddieAnita <maddie@mallingmay.com>
2024-05-28 13:59:10 +0100

    makes changes according to feedback, fixing test and ensuring response has no key

--- models/endpoints.models.js
+++ models/endpoints.models.js
@@ -1,5 +1,4 @@
 const fs = require("fs/promises");
-const { dirname } = require("path");
 
 exports.findApiEndpoints = () => {
   return fs.readFile(`${__dirname}/../endpoints.json`, "utf-8").then((data) => {
