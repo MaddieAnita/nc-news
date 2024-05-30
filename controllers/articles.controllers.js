d24ef1cfca8c4dd02cd3866cdc2e7516aaa8370a
MaddieAnita <maddie@mallingmay.com>
2024-05-30 10:12:26 +0100

    amends logic on queries, ignores queries not yet built but protects against invalid query options

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -5,7 +5,8 @@ const {
 } = require("../models/articles.models");
 
 exports.getArticles = (req, res, next) => {
-  findArticles(req.query)
+  const { sort_by, order, topic } = req.query;
+  findArticles(sort_by, order, topic)
     .then((articles) => {
       res.status(200).send({ articles });
     })
