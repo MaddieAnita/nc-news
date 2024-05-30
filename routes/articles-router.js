55e392877940b8bb10f6a8d99fd4a0625d46be97
MaddieAnita <maddie@mallingmay.com>
2024-05-30 11:11:43 +0100

    modifies app.js to make use of express routing

---
+++ routes/articles-router.js
@@ -0,0 +1,25 @@
+const {
+  getArticles,
+  getArticlesById,
+  patchArticleVotesById,
+} = require("../controllers/articles.controllers");
+const {
+  getCommentsByArticleId,
+  postCommentByArticleId,
+} = require("../controllers/comments.controllers");
+
+const articlesRouter = require("express").Router();
+
+articlesRouter.route("/").get(getArticles);
+
+articlesRouter
+  .route("/:article_id")
+  .get(getArticlesById)
+  .patch(patchArticleVotesById);
+
+articlesRouter
+  .route("/:article_id/comments")
+  .get(getCommentsByArticleId)
+  .post(postCommentByArticleId);
+
+module.exports = articlesRouter;
