95a5c92abfdca6e62ef3d8dcbafaffa1dfdd3a0f
MaddieAnita <maddie@mallingmay.com>
2024-06-17 10:14:41 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const {
   handleServerErrors,
   handleCustomErrors,
@@ -7,6 +8,7 @@ const {
 
 const apiRouter = require("./routes/api-router");
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
